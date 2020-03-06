6ac02506d8b3805e746fa5c58bd7c00ed09e3435
null32sys <coldcloud87@gmail.com>
2020-03-07 06:32:30 +0700

    Ubah Hello index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello Triasse Dev Team! Edited By Lukman</h1>')
+    res.send('<h1>Hello Triasse Dev Team! Edited By Lukman v2</h1>')
 })
 
 const PORT = process.env.PORT || 8080
